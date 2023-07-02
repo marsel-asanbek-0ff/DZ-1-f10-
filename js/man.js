4aa5f51115b911c99e6dd3ca49654c823bb3e866
marsel-asanbek-0ff <bajdalievmarsel@gmail.com>
2023-07-03 00:33:47 +0600

    ex-1

---
+++ js/man.js
@@ -0,0 +1,9 @@
+    var firstName = prompt("Введите ваше имя:"); 
+    
+    var lastName = prompt("Введите вашу фамилию:");
+    
+    var greeting = "Привет, " + firstName + " " + lastName + "!";
+  
+    document.write("<h1>" + greeting + "</h1>");
+
+    console.log(greeting);
